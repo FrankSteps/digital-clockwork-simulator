c31b018b8613e790729f1c516f0a6040eb53d064
FrankSteps <franciscopassos.contato@gmail.com>
2026-04-03 17:16:54 -0300

    feat: implement Display and LED classes for seven-segment and LED output

---
+++ include/feedback.hpp
@@ -0,0 +1,36 @@
+/*
+
+*/
+
+#ifndef DISPLAY_HPP
+#define DISPLAY_HPP
+
+#include <array>
+#include <string>
+#include <iostream>
+
+// Seven-segments display
+class Display{
+    private:
+        int decodeDigit(const std::array<bool, 7>& segments);
+
+    public:
+        Display() = default;
+        std::string render(const std::array<std::array<bool,7>,4>& segments);
+};
+
+
+// Led
+class Led{
+    private:
+        bool state  = false;
+
+    public:
+        Led() = default;
+
+        void setState(bool value);
+        char getState();
+};
+
+
+#endif 
---
+++ src/feedback.cpp
@@ -0,0 +1,60 @@
+#include "feedback.hpp"
+
+#include <iostream>
+
+/*
+    DISPLAY
+*/
+
+// Decodes a 7-segment pattern into its corresponding digit (0–9)
+int Display::decodeDigit(const std::array<bool, 7>& segments){
+    
+    if  (segments == std::array<bool,7>  {1,1,1,1,1,1,0})  return 0;
+    if  (segments == std::array<bool,7>  {0,1,1,0,0,0,0})  return 1;
+    if  (segments == std::array<bool,7>  {1,1,0,1,1,0,1})  return 2;
+    if  (segments == std::array<bool,7>  {1,1,1,1,0,0,1})  return 3;
+    if  (segments == std::array<bool,7>  {0,1,1,0,0,1,1})  return 4;
+    if  (segments == std::array<bool,7>  {1,0,1,1,0,1,1})  return 5;
+    if  (segments == std::array<bool,7>  {1,0,1,1,1,1,1})  return 6;
+    if  (segments == std::array<bool,7>  {1,1,1,0,0,0,0})  return 7;
+    if  (segments == std::array<bool,7>  {1,1,1,1,1,1,1})  return 8;
+    if  (segments == std::array<bool,7>  {1,1,1,1,0,1,1})  return 9;
+
+    return -1;
+}
+
+// Converts 4 seven-segment digits into a string (HH:MM)
+std::string Display::render(const std::array<std::array<bool,7>,4>& segments){
+    std::string output;
+
+    for(int i = 3; i >= 0; i--){
+        int num = decodeDigit(segments[i]);
+        output += std::to_string(num);
+    
+        if(i == 2){
+            output += ":"; 
+        }
+    }
+
+    return output;
+}
+
+
+
+/*
+    LED
+*/
+
+// Set the LED state
+void Led::setState(bool value){
+    state = value;
+}
+
+// Returns a character representing the LED state
+char Led::getState(){
+    if(state){
+        return '◉';
+    }
+
+    return '◎';
+}

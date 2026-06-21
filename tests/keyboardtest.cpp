d156cdd41738a9326bea7d3d80fcf7eb87ac78be
FrankSteps <franciscopassos.contato@gmail.com>
2026-06-20 21:57:04 -0300

    fix: add keyboardtest.cpp again

---
+++ tests/keyboardtest.cpp
@@ -0,0 +1,41 @@
+/*
+
+*/
+
+#include <libevdev/libevdev.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <iostream>
+
+
+int main(){
+    int fd = open("/dev/input/event4", O_RDONLY | O_NONBLOCK);
+
+    if(fd < 0){
+        std::cerr << "\n";
+        return 1;
+    }
+
+    struct libevdev* dev = nullptr;
+    libevdev_new_from_fd(fd, &dev);
+
+
+    while(true){
+        struct input_event ev;
+
+        int rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL, &ev);
+
+        if(rc == 0 && ev.type == EV_KEY){
+            if(ev.value == 1){
+                std::cout << "Pressed: " << libevdev_event_code_get_name(EV_KEY, ev.code) << "\n";
+            } else if(ev.value == 0) {
+                std::cout << ": "       << libevdev_event_code_get_name(EV_KEY, ev.code) << "\n";
+            }
+        }
+    }
+
+    libevdev_free(dev);
+    close(fd);
+
+    return 0;
+}
